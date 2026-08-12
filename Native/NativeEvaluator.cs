449506889d2c9e94823350a64574ff1f38a02b56
Aliz Pasztor <al1zp4sztor@gmail.com>
2026-08-12 15:29:29 +0100

    Modified gitignore file

---
+++ Native/NativeEvaluator.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.InteropServices;
+
+namespace RTPCCurveEditor.Native;
+
+public static class NativeEvaluator
+{
+    private const string DllName = "RTPCCurveEvaluatorNative.dll";
+
+    [DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
+    public static extern double EvaluateCubicBezierYAtX(
+        double p0x, double p0y,
+        double c0x, double c0y,
+        double c1x, double c1y,
+        double p1x, double p1y,
+        double targetX);
+}
